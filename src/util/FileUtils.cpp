ca6b39fe085f520bb77f2b585e7aa463e167d7eb
博麗靈夢 <1725338233@qq.com>
2022-05-10 21:18:05 +0800

    增加 注释
    Signed-off-by: 博麗靈夢 <1725338233@qq.com>

--- src/util/FileUtils.cpp
+++ src/util/FileUtils.cpp
@@ -16,7 +16,9 @@ using namespace std;
  * @return 转换后的 cJson
  */
 cJSON *student2JSON(const Student &student) {
+    // 创建 JSON 对象
     cJSON *json = cJSON_CreateObject();
+    // 将 Student 属性作为 key 存入 JSON
     cJSON_AddStringToObject(json, "name", student.getCharName());
     cJSON_AddStringToObject(json, "address", student.getCharAddress());
     cJSON_AddStringToObject(json, "phone", student.getCharPhone());
@@ -34,27 +36,46 @@ cJSON *student2JSON(const Student &student) {
 void saveStudents(Student students[], int num, const char *saveFile = "save.json") {
     // 打开并清空文件
     ofstream file(saveFile, ios::trunc);
+    // 创建 JSON 对象
     cJSON *json = cJSON_CreateObject();
+    // 存入一个 Array 对象, key 为 students
     cJSON *array = cJSON_AddArrayToObject(json, "students");
+    // 遍历 students 数组
     for (int i = 0; i < num; i++) {
+        // 将 序列化后的 student 对象存入 array
         cJSON_AddItemToArray(array, student2JSON(students[i]));
     }
+    // 将 JSON 数据写入到文件
     file << cJSON_Print(json) << endl;
+    // 释放资源
     file.close();
     cJSON_free(array);
     cJSON_free(json);
 }
 
 
+/**
+ * 反序列化 Student 数组
+ * @param size 反序列化后的 Student 数组长度
+ * @param saveFile 保存的文件名
+ * @return 反序列化后的 Student 数组
+ */
 Student *readStudents(int &size, const char *saveFile = "save.json") {
+    // 打开文件
     ifstream file(saveFile, ios::in);
+    // 使用 stringStream 一次性将所有内容读入内存
     stringstream buffer;
     buffer << file.rdbuf();
     string contents(buffer.str());
+    // 解析 JSON 字符串
     cJSON *json = cJSON_Parse(contents.c_str());
+    // 取出 students 数组
     cJSON *array = cJSON_GetObjectItem(json, "students");
+    // 获取 students 长度并将其赋值给 size
     size = cJSON_GetArraySize(array);
+    // 创建 size 个 Student 对象
     auto *students = new Student[size];
+    // 遍历 JSON 节点, 并给 student 赋值
     for (int i = 0; i < size; ++i) {
         cJSON *student = cJSON_GetArrayItem(array, i);
         students[i].setName(cJSON_GetArrayItem(student, 0)->valuestring);
