0a8b7d1b4d9ce52348d7c82a7cdd4ddef3450f78
博麗靈夢 <1725338233@qq.com>
2022-05-16 13:00:22 +0800

    增加 students.sql
    Signed-off-by: 博麗靈夢 <1725338233@qq.com>

---
+++ students.sql
@@ -0,0 +1,10 @@
+-- auto-generated definition
+create table students
+(
+    sid      integer not null primary key,
+    name     TEXT,
+    address  TEXT,
+    phone    TEXT,
+    postCode TEXT,
+    "E-mail" TEXT
+);
