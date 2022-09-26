be1819f30fbaecfc4502d141c3dc3a2b534c6e62
博麗靈夢 <1725338233@qq.com>
2022-09-26 12:41:51 +0800

    增加 虚拟数据
    Signed-off-by: 博麗靈夢 <1725338233@qq.com>

--- students.sql
+++ students.sql
@@ -1,10 +1,9 @@
--- auto-generated definition
 create table students
 (
     sid      INTEGER PRIMARY KEY AUTOINCREMENT,
     name     TEXT,
     address  TEXT,
-    phone    INTEGER CHECK ( length(phone) = 11 ),
+    phone    INTEGER CHECK ( length(phone) >= 8 ),
     postCode INTEGER CHECK ( length(0 + postCode) = length(postCode) ),
     email    TEXT CHECK ( instr(email, '@') != 0 )
 );
